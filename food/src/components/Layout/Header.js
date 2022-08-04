b4310fba7fd3b9f107a006986d92c0d06851d2d1
Mount-Blanc <mountblanc87@gmail.com>
2022-08-04 10:19:50 -0700

    created meals files

--- food/src/components/Layout/Header.js
+++ food/src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import { Fragment} from 'react';
-
+import HeaderCartButton from './HeaderCartButton';
 import vagaImage from '../../assets/vaga.jpg';
 
 const Header = props => {
     return <Fragment>
         <header>
             <h1>ReactMeals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div>
             <img src={vagaImage} alt='musashi eating'/>
---
+++ food/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderCartButton = (props) => {
+return <button>
+    <span>
+
+    </span>
+    <span>Your Cart</span>
+    <span>3</span>
+</button>
+}
+
+export default HeaderCartButton;
